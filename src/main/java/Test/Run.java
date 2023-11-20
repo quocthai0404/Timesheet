2e3a5ce0b04c406130695587c9f44430d1c5c82d
quocthai0404 <thaiphan0804@gmail.com>
2023-11-20 12:48:11 +0700

    work_s_dao

--- src/main/java/Test/Run.java
+++ src/main/java/Test/Run.java
@@ -9,40 +9,52 @@ import java.util.List;
 import entity.Attendance;
 
 import DAO.EmployeeDAO;
-
+import DAO.Work_scheduleDAO;
 import entity.Employee;
 import entity.Leave;
 import entity.Salary;
+import entity.Work_schedule;
 public class Run {
 	public static void main(String[] args) {
-
-		 String dateString = "2000-12-31 12:09:07";
-	        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-
-	        try {
-	            java.util.Date parsedDate = dateFormat.parse(dateString);
-	            Timestamp timestamp = new Timestamp(parsedDate.getTime());
-
-	            // Sử dụng timestamp theo nhu cầu của bạn
-	            System.out.println("Timestamp: " + timestamp);
-	            Attendance att = new Attendance(1,1,1,timestamp,timestamp,15.6,"abc");
-	            System.out.println(att.toString());
-	        } catch (ParseException e) {
-	            e.printStackTrace();
-	        }
+		List<Work_schedule> list = new ArrayList<>();
+		Work_scheduleDAO dao = new Work_scheduleDAO();
+		list = dao.selectWorkSchedule();
+		list.forEach(wc -> System.out.println(wc.toString()));
 		
-		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-		Salary sl = new Salary();
-		System.out.println(sl);
+		
+		
+		
 		
 		
 		
 
-
-		EmployeeDAO empdao = new EmployeeDAO();
-		List<Employee> list = new ArrayList<>();
-		list = empdao.selectEmployee();
-		list.forEach(emp -> System.out.println(emp.toString()));
+//		 String dateString = "2000-12-31 12:09:07";
+//	        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+//
+//	        try {
+//	            java.util.Date parsedDate = dateFormat.parse(dateString);
+//	            Timestamp timestamp = new Timestamp(parsedDate.getTime());
+//
+//	            // Sử dụng timestamp theo nhu cầu của bạn
+//	            System.out.println("Timestamp: " + timestamp);
+//	            Attendance att = new Attendance(1,1,1,timestamp,timestamp,15.6,"abc");
+//	            System.out.println(att.toString());
+//	        } catch (ParseException e) {
+//	            e.printStackTrace();
+//	        }
+//		
+//		SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+//		Salary sl = new Salary();
+//		System.out.println(sl);
+//		
+//		
+//		
+//
+//
+//		EmployeeDAO empdao = new EmployeeDAO();
+//		List<Employee> list = new ArrayList<>();
+//		list = empdao.selectEmployee();
+//		list.forEach(emp -> System.out.println(emp.toString()));
 
 
 		
