03f932739d162ab82f64eb835e6db067091bbf4c
baonghesi <baotrinh495@gmail.com>
2023-11-21 13:40:33 +0700

    'nbao'

--- src/main/java/DAO/SalaryDAO.java
+++ src/main/java/DAO/SalaryDAO.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+
+import javax.swing.JOptionPane;
 
 import database.JdbcUlti;
 import entity.Salary;
@@ -33,5 +36,55 @@ public class SalaryDAO {
             e.printStackTrace();
         }
         return list;
+    }
+    public void deleteSalary(int salary_id) {
+    	try {
+    		Connection con = JdbcUlti.getConnection();
+            String sql = "delete from salary where salary_id=?";
+            try (PreparedStatement statement = con.prepareStatement(sql)) {
+                statement.setInt(1, salary_id);
+                statement.executeUpdate();
+            }
+            JdbcUlti.closeConnection(con);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
+    }
+    
+    public void updateSalary(int salary_id, int employee_id, double base_salary, double overtime_pay, double paid_leave_pay, double allowance) {
+        try {
+            Connection con = JdbcUlti.getConnection();
+            String sql = "update salary set employee_id = ?, base_salary = ?, overtime_pay = ?, paid_leave_pay = ?, allowance = ? WHERE salary_id = ?";
+            try (PreparedStatement statement = con.prepareStatement(sql)) {
+                statement.setInt(1, employee_id);
+                statement.setDouble(2, base_salary);
+                statement.setDouble(3, overtime_pay);
+                statement.setDouble(4, paid_leave_pay);
+                statement.setDouble(5, allowance);
+                statement.setInt(6, salary_id);
+                statement.executeUpdate();
+            }
+            JdbcUlti.closeConnection(con);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    public void addSalary(int employee_id, double base_salary, double overtime_pay, double paid_leave_pay, double allowance) {
+    	try {
+    		Connection con = JdbcUlti.getConnection();
+            String sql = "insert into salary (employee_id, base_salary, overtime_pay, paid_leave_pay, allowance) values (?,?,?,?,?)";
+            try (PreparedStatement statement = con.prepareStatement(sql)){
+            	statement.setInt(1, employee_id);
+            	statement.setDouble(2, base_salary);
+                statement.setDouble(3, overtime_pay);
+                statement.setDouble(4, paid_leave_pay);
+                statement.setDouble(5, allowance);
+                statement.executeUpdate();
+            }
+            JdbcUlti.closeConnection(con);
+    	} catch (Exception e) {
+			e.printStackTrace();
+		}
     }
 }
