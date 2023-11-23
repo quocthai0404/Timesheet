f5fb4ab3fad793be92c234f35bde6954f39da821
Your Name <you@example.com>
2023-11-23 12:50:11 +0700

    loc

--- src/main/java/DAO/Salary_deductionDAO.java
+++ src/main/java/DAO/Salary_deductionDAO.java
@@ -5,39 +5,17 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import database.JdbcUlti;
 import entity.Salary_deduction;
 
 public class Salary_deductionDAO {
-<<<<<<< HEAD
 
-
-=======
->>>>>>> 1786c1d4a4e876aa82d0937fe6fe2ab1a7b5678d
-    
     public List<Salary_deduction> selectAllSalaryDeductions() {
         List<Salary_deduction> list = new ArrayList<>();
         try {
             Connection con = JdbcUlti.getConnection();
-<<<<<<< HEAD
-    		var statement = con.createStatement();
-    		String query = "SELECT * FROM salary_deduction";
-    		ResultSet rs = statement.executeQuery(query);
-               while (rs.next()) {
-
-                    list.add(new Salary_deduction(
-                            rs.getInt("salary_deduciton_id"),
-                            rs.getInt("employee_id"),
-                            rs.getString("deduction_reason"),
-                            rs.getDouble("deduction_amount"),
-                            rs.getDate("deduction_date")
-                    ));
-
-           }
-           JdbcUlti.closeConnection(con);
-
-=======
             var statement = con.createStatement();
             String query = "SELECT * FROM salary_deduction";
             ResultSet rs = statement.executeQuery(query);
@@ -51,7 +29,6 @@ public class Salary_deductionDAO {
                 ));
             }
             JdbcUlti.closeConnection(con);
->>>>>>> 1786c1d4a4e876aa82d0937fe6fe2ab1a7b5678d
         } catch (SQLException e) {
             e.printStackTrace();
         }
